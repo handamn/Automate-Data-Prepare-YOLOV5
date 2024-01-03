04947bf357c2d4a35be334337c6005e4fa45ea3e
handamn <kupretzzz14@gmail.com>
2024-01-03 08:00:25 +0700

    Rev4 Combine Script

--- 4_Program/Combine.py.py
+++ 4_Program/Combine.py.py
@@ -6,21 +6,13 @@ from tqdm import tqdm
 import shutil
 import random
 
-"""out_or_in       = "Out_line"
+############################
+out_or_in       = "Out_line"
 nama_folder     = "4_ACDF" 
 tipe_kendaraan  = "Innova_RHD"
 box_ke          = "Box1"
-basis_ke        = "train1"""
-
 basis_ke        = "train1"
-
-out_or_in = input("Masukkan Area : ")
-kendaraan = input("Masukkan Mobil : ")
-steer = input("Masukkan Steer : ")
-tipe_kendaraan = kendaraan + "_" + steer
-box_ke = input("Masukkan Boxke : ")
-nama_folder = input("Masukkan nama folder : ")
-
+############################
 
 def baca_file_csv(nama_file):
     data = []
@@ -30,92 +22,97 @@ def baca_file_csv(nama_file):
             data.append(baris[0])  # Ambil elemen pertama dari setiap baris
     return data
 
+def split_data(val, input_folder, output_folder, num_samples=200, split_ratio=0.8, random_seed=42):
+    random.seed(random_seed)
+
+    folder_gambar = os.path.join(input_folder, "images")
+    folder_label = os.path.join(input_folder, "labels")
+
+    gabung = os.path.join(output_folder, val)
+    print(gabung)
+
+    folder_pelatihan = os.path.join(gabung, "train")
+    folder_gambar_pelatihan = os.path.join(folder_pelatihan, "images")
+    folder_label_pelatihan = os.path.join(folder_pelatihan, "labels")
+
+    folder_validasi = os.path.join(gabung, "val")
+    folder_gambar_validasi = os.path.join(folder_validasi, "images")
+    folder_label_validasi = os.path.join(folder_validasi, "labels")
+
+    # Buat folder keluaran jika belum ada
+    for folder in [folder_pelatihan, folder_gambar_pelatihan, folder_label_pelatihan,
+                   folder_validasi, folder_gambar_validasi, folder_label_validasi]:
+        os.makedirs(folder, exist_ok=True)
+
+    # Dapatkan daftar gambar di folder input
+    daftar_gambar = os.listdir(folder_gambar)
+
+    # Ambil secara acak 200 sampel
+    sampled_images = random.sample(daftar_gambar, num_samples)
+
+    # Hitung jumlah gambar untuk pelatihan
+    num_train = int(len(sampled_images) * split_ratio)
+
+    # Salin gambar dan label ke folder pelatihan
+    with tqdm(total=num_train, desc="Copying training data") as pbar:
+        for nama_gambar in sampled_images[:num_train]:
+            path_gambar = os.path.join(folder_gambar, nama_gambar)
+            path_label = os.path.join(folder_label, nama_gambar.replace(".jpg", ".txt"))
+
+            shutil.copy(path_gambar, folder_gambar_pelatihan)
+            shutil.copy(path_label, folder_label_pelatihan)
+
+            pbar.update(1)
+
+    # Salin gambar dan label ke folder validasi
+    with tqdm(total=len(sampled_images) - num_train, desc="Copying validation data") as pbar:
+        for nama_gambar in sampled_images[num_train:]:
+            path_gambar = os.path.join(folder_gambar, nama_gambar)
+            path_label = os.path.join(folder_label, nama_gambar.replace(".jpg", ".txt"))
+
+            shutil.copy(path_gambar, folder_gambar_validasi)
+            shutil.copy(path_label, folder_label_validasi)
+
+            pbar.update(1)
+
+# Contoh penggunaan dengan 200 sampel acak
+#split_data(folder_input, folder_output, num_samples=200, split_ratio=0.8, random_seed=42)
+
 nama_file_csv = '/home/pcsistem/camera_vision_develop/2_Stock_Foto/Out_line/Innova_RHD/Box1/index_kelas.csv'  # Ganti dengan nama file CSV Anda
 data_csv = baca_file_csv(nama_file_csv)
 
-Kode_Box = input("Enter your value: ")
+############################
+base_folder = '/home/pcsistem/camera_vision_develop/2_Stock_Foto/'
+folder_inline = base_folder + 'In_line/' + tipe_kendaraan + '/' + box_ke + '/' + nama_folder + '/'
+#folder_outline = base_folder + 'Out_line/' + tipe_kendaraan + '/' + box_ke + '/' + nama_folder + '/'
+############################
 
-if Kode_Box in data_csv:
-    index = data_csv.index(Kode_Box)
-    print(f"{Kode_Box} ditemukan pada indeks {index}")
+val = input("Enter your value: ")
+
+if val in data_csv:
+    index = data_csv.index(val)
+    print(f"{val} ditemukan pada indeks {index}")
     print(True)
     # Lakukan operasi lain berdasarkan indeks jika diperlukan
     ############################
 
+    folder_outline = base_folder + 'Out_line/' + tipe_kendaraan + '/' + box_ke + '/' + val + '/'
 
 
-    folder_train_images = '/home/pcsistem/camera_vision_develop/2_Stock_Foto/' + out_or_in + '/' + tipe_kendaraan + '/' + box_ke + '/' + nama_folder + '/images/'
-    folder_train_labels = '/home/pcsistem/camera_vision_develop/2_Stock_Foto/' + out_or_in + '/' + tipe_kendaraan + '/' + box_ke + '/' + nama_folder + '/labels/'
+    folder_images_Outline = folder_outline + 'images/'
+    folder_labels_Outline = folder_outline + 'labels/'
 
-    folder_model = '/home/pcsistem/camera_vision_develop/2_Stock_Foto/' + out_or_in + '/' + tipe_kendaraan + '/' + box_ke + '/' + nama_folder + '/models/' + basis_ke + '/weights/best.pt'
-
-    model = torch.hub.load('.', "custom", path = folder_model, source='local', force_reload=True)
-
-    direktori_class = "/home/pcsistem/camera_vision_develop/2_Stock_Foto/Out_line/Innova_RHD/Box1/index_kelas.txt"
-
-    with open(direktori_class, "r") as f:
-        classes_list = [line.strip().replace("X_","") for line in f.readlines()]
 
     # Mendapatkan daftar nama file di dalam folder
-    train_list = os.listdir(folder_train_images)
+    image_list = os.listdir(folder_images_Outline)
+    i = 0
 
-    # Mencetak nama file di dalam folder
-    for nama in tqdm(train_list, desc="Processing Images"):
+    split_data(val,folder_outline, "/home/pcsistem/camera_vision_develop/4_Program/coba", num_samples=250, split_ratio=0.8, random_seed=42)
 
-        # Membagi string menjadi list
-        namafile = nama.split(".")
-
-        # Mengambil string pertama
-        namafile_clean = namafile[0]
-
-        nama_file_txt = namafile_clean + ".txt"
-        direktori_label = folder_train_labels + "/" + nama_file_txt
-        #print(nama_file_txt)
-
-        direktori_image = folder_train_images + "/" + nama
-        results = model(direktori_image)
-        hasil = results.pandas().xyxy[0]
-
-        if (hasil.empty == False):
-            xmin = hasil['xmin'][0]
-            ymin = hasil['ymin'][0]
-            xmax = hasil['xmax'][0]
-            ymax = hasil['ymax'][0]
-            kelas = hasil['class'][0]
-
-            kelas_index = classes_list.index(str(nama_folder))
-
-            xcenter = ((xmax+xmin)/2)/640
-            ycenter = ((ymax+ymin)/2)/480
-            width = (xmax-xmin)/640
-            height = (ymax-ymin)/480
-
-
-            text = str(index) + " " + str(xcenter) + " " + str(ycenter) + " " + str(width) + " " + str(height)
-
-            with open(direktori_label, "w") as f:
-                f.write(text)
-            
-
-            #bikin file class
-            new_kelas = nama_folder[3:]
-            nama_file_kelas_txt = "classes.txt"
-            direktori_kelas = folder_train_labels + "/" + nama_file_kelas_txt
-
-            """with open(direktori_kelas, "w") as f:
-                f.write(new_kelas)"""
-                
-        else :
-            os.remove(direktori_image)
-            continue
-        
-        
     print("ALHAMDULILLAH SELESAI")
     print(" ")
 
 
 else:
-    print(f"{Kode_Box} tidak ditemukan dalam data_csv")
+    print(f"{val} tidak ditemukan dalam data_csv")
     print(False)
-
-
