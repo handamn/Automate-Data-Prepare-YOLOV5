5c53bd5aa28cb6bbace162d2021a4013f10db6d9
handamn <kupretzzz14@gmail.com>
2024-01-05 08:36:32 +0700

    Rev7 Combine Script

--- 4_Program/Combine.py
+++ 4_Program/Combine.py
@@ -5,6 +5,7 @@ import time
 from tqdm import tqdm
 import shutil
 import random
+import yaml
 
 ############################
 print(" ")
@@ -28,7 +29,21 @@ def baca_file_csv(nama_file):
             data.append(baris[0])  # Ambil elemen pertama dari setiap baris
     return data
 
-def split_data(kode_box, input_folder, output_folder, num_samples=200, split_ratio=0.8, random_seed=42):
+def process_label_file(file_path, new_value):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    if lines:
+        # Split data and modify the first value
+        data = lines[0].strip().split(' ')
+        data[0] = str(new_value)
+
+        # Join the modified data and overwrite the file
+        modified_data = ' '.join(data)
+        with open(file_path, 'w') as file:
+            file.write(modified_data)
+
+def split_data(kode_box, input_folder, output_folder, num_samples=200, split_ratio=0.8, new_value = 100,random_seed=42):
     random.seed(random_seed)
 
     folder_gambar = os.path.join(input_folder, "images")
@@ -64,6 +79,8 @@ def split_data(kode_box, input_folder, output_folder, num_samples=200, split_rat
             path_gambar = os.path.join(folder_gambar, nama_gambar)
             path_label = os.path.join(folder_label, nama_gambar.replace(".jpg", ".txt"))
 
+            process_label_file(path_label,new_value)
+
             shutil.copy(path_gambar, folder_gambar_pelatihan)
             shutil.copy(path_label, folder_label_pelatihan)
 
@@ -74,6 +91,8 @@ def split_data(kode_box, input_folder, output_folder, num_samples=200, split_rat
         for nama_gambar in sampled_images[num_train:]:
             path_gambar = os.path.join(folder_gambar, nama_gambar)
             path_label = os.path.join(folder_label, nama_gambar.replace(".jpg", ".txt"))
+
+            process_label_file(path_label, new_value)
 
             shutil.copy(path_gambar, folder_gambar_validasi)
             shutil.copy(path_label, folder_label_validasi)
@@ -102,17 +121,17 @@ if kode_box in data_csv:
         if x != kode_box :
             i+=1
             folder_outline_clean = base_folder + 'Out_line/' + kendaraan + '/' + box + '/' + x
-            split_data(kode_box,folder_outline_clean, folder_output, num_samples=NG_count, split_ratio=percentage, random_seed=42)
+            split_data(kode_box,folder_outline_clean, folder_output, num_samples=NG_count, split_ratio=percentage, new_value = 0, random_seed=42)
     ########################
 
     ##### Inline Clean #####
     folder_inline = base_folder + 'In_line/' + kendaraan + '/' + box + '/' + kode_box + '/'
-    split_data(kode_box,folder_inline, folder_output, num_samples=OK_count, split_ratio=percentage, random_seed=42)
+    split_data(kode_box,folder_inline, folder_output, num_samples=OK_count, split_ratio=percentage, new_value = 1, random_seed=42)
     ########################
 
     ##### Outline Clean #####
     folder_outline = base_folder + 'Out_line/' + kendaraan + '/' + box + '/' + kode_box + '/'
-    split_data(kode_box,folder_outline, folder_output, num_samples=OK_count, split_ratio=percentage, random_seed=42)
+    split_data(kode_box,folder_outline, folder_output, num_samples=OK_count, split_ratio=percentage, new_value = 1, random_seed=42)
     ########################
 
     print(" ")
@@ -131,10 +150,25 @@ if kode_box in data_csv:
     print(" ")
     print("#################################################")
 
-
-
-
 else:
     print(f"{kode_box} tidak ditemukan dalam data_csv")
     print(False)
 
+
+#### Pembuatan file yaml
+
+data = {
+    'train': folder_output + "/" + kode_box + '/train',
+    'val': folder_output + "/" + kode_box + '/val',
+    'names': {
+        0: 'DATA_SALAH',
+        1: kode_box
+    }
+}
+
+file_path = folder_output + "/" + kode_box + "/" + kode_box +'.yaml'  # Ganti dengan nama file YAML yang diinginkan
+
+with open(file_path, 'w') as file:
+    yaml.dump(data, file, default_flow_style=False, sort_keys=False)
+
+print(f"File YAML telah dibuat: {file_path}")
